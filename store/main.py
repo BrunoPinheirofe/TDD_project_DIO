1e2d350d33b15dcff72b68532b22b43c096beffc
BrunoPinheirofe <bruno2013pinheiro@gmail.com>
2024-07-01 15:24:57 -0300

    commit final

--- store/main.py
+++ store/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from store.core.config import settings
+from store.routers import api_router
 
 
 class App(FastAPI):
@@ -14,3 +15,4 @@ class App(FastAPI):
 
 
 app = App()
+app.include_router(api_router)
